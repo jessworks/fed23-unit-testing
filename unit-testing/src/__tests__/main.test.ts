d1a878460514b55f274022cf6d5c0aeb386c07ca
jessworks <48979913+jessworks@users.noreply.github.com>
2024-05-19 06:10:50 +0200

    passed test

--- unit-testing/src/__tests__/main.test.ts
+++ unit-testing/src/__tests__/main.test.ts
@@ -3,13 +3,13 @@ import { sendMessage } from "../main";
 describe ('main function', () => {
 
     test('should be what goes in', () => {
-        
+
         //assign
-        const theMessage: string = 'hello';
-        let outcome: string = '';
+        const theMessage = 'hello';
+        
 
         //act
-        outcome = sendMessage(theMessage);
+        let outcome = sendMessage(theMessage);
 
         //assert
         expect(outcome).toBe('hello');
